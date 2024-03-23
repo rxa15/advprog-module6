867c9faa5168dfa69c7998d1230d4822c7f0ae71
rxa15 <114054405+rxa15@users.noreply.github.com>
2024-03-23 15:44:36 +0700

    (1) Handle-connection, check response

---
+++ target/debug/deps/hello.d
@@ -0,0 +1,5 @@
+C:\Users\ASUSTeK\Documents\Fasilkom\SEM 4\Adpro\Tutorial 6\hello\target\debug\deps\hello.exe: src\main.rs
+
+C:\Users\ASUSTeK\Documents\Fasilkom\SEM 4\Adpro\Tutorial 6\hello\target\debug\deps\hello.d: src\main.rs
+
+src\main.rs:
---
+++ target/debug/hello.d
@@ -0,0 +1,1 @@
+C:\Users\ASUSTeK\Documents\Fasilkom\SEM\ 4\Adpro\Tutorial\ 6\hello\target\debug\hello.exe: C:\Users\ASUSTeK\Documents\Fasilkom\SEM\ 4\Adpro\Tutorial\ 6\hello\src\main.rs
